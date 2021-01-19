f43eab4a76b5a1eb5d7d21f27e60d74f000d6fc2
snh15978 <snh15978@gmail.com>
2021-01-19 15:38:58 +0900

    부트스트랩 + 로그인폼

---
+++ src/LoginForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {Form, Button} from 'react-bootstrap';
+
+class LoginForm extends Component {
+    render() {
+        const formStyle = {
+            margin: 50
+        };
+        return (
+            <Form style={formStyle}>
+                <Form.Group controlId="joinForm">
+                    <Form.Label>ID</Form.Label>
+                    <Form.Control type="text" ref={ref => (this.joinID = ref)} placeholder="ID"/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                    <Form.Label>NAME</Form.Label>
+                    <Form.Control type="text" ref={ref => (this.joinName = ref)}  placeholder="NAME"/>
+                    <Form.Label>PASSWORD</Form.Label>
+                    <Form.Control type="password" ref={ref => (this.joinPw = ref)}  placeholder="Password"/>
+                    <Button variant="primary" type="button">회원가입</Button>
+                </Form.Group>
+                <Form.Group controlId="loginForm">
+                    <Form.Label>ID</Form.Label>
+                    <Form.Control type="text" ref={ref => (this.loginID = ref)}  placeholder="ID"/>
+                    <Form.Label>PASSWORD</Form.Label>
+                    <Form.Control type="password" ref={ref => (this.loginPw = ref)}   placeholder="Password"/>
+                    <Button variant="primary" type="button">로그인</Button>
+                </Form.Group>
+            </Form>
+        )
+    }
+}
+
+export default LoginForm;
